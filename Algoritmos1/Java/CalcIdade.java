7f669eaf6826efe994ad24b69d3947453c17d782
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2020-02-20 14:09:16 -0300

    1.1

--- Algoritmos1/Java/CalcIdade.java
+++ Algoritmos1/Java/CalcIdade.java
@@ -7,7 +7,16 @@ public class CalcIdade {
 		String nome;
 		int anoNasc, anoAtual, idade;
 		Scanner teclado = new Scanner(System.in);
-		System.out.print("Digite o seu nome:");
+		System.out.print("Digite o seu nome: ");
+		nome = teclado.nextLine();
+		System.out.print("Digite o seu ano de nascimento: ");
+		anoNasc = teclado.nextInt();
+		System.out.print("Digite o ano atual: ");
+		anoAtual = teclado.nextInt();
+
+		idade = anoAtual - anoNasc;
+		System.out.println(nome+", voce tem/terá "+idade+" anos em "+anoAtual);
+		teclado.close();
 
 	}
 
