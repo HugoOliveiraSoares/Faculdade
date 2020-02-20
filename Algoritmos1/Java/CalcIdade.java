13e3d97e779be3eaa27991792480e57674deee2a
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2020-02-20 12:38:48 -0300

    1.0

---
+++ Algoritmos1/Java/CalcIdade.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class CalcIdade {
+
+	public static void main(String[] args) {
+
+		String nome;
+		int anoNasc, anoAtual, idade;
+		Scanner teclado = new Scanner(System.in);
+		System.out.print("Digite o seu nome:");
+
+	}
+
+}
