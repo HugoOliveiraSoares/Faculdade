1bc6f6032c9804583b4e5923e948801b00c65e85
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2020-08-19 21:50:20 -0300

    Lista de revisão de AED1
    Apresenta o valor absoluto de um valor real

---
+++ Algoritmos2/lista0/valorabsoluto.java
@@ -0,0 +1,14 @@
+public class valorabsoluto {
+    public static void main(String[] args) {
+
+        ValorAbsoluto(-3.1478);
+
+    }
+
+    public static void ValorAbsoluto(double numero) {
+
+        double varAbs = Math.abs(numero);
+
+        System.out.println("Valor absoluto: " + varAbs);
+    }
+}
