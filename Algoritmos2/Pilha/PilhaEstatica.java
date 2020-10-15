6dc22dd98d946bda548fa5c833be53c510ac6321
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2020-10-15 08:07:14 -0300

    Estudo de pilhas dinamicas e estaticas

---
+++ Algoritmos2/Pilha/PilhaEstatica.java
@@ -0,0 +1,72 @@
+/**
+ * PilhaEstatica
+ */
+public class PilhaEstatica {
+
+    private int topo; // Indicador do topo da pilha
+    private int contador; // Contador de itens na pilha
+    private int capacidade; // Capacidade macima da pilha
+    private Object[] itemArray; // Armazenador de objetos/itens/elementos
+
+    // Quando na calsse main está instanciado sem um parametro, executa este construtor com o tamanho 10
+    public PilhaEstatica(){ // Construtor
+        this(10); //PilhaEstatica(10)
+    }
+
+    public PilhaEstatica( int maxCapacidade ){ // Construtor
+        topo = 0;
+        contador = 0;
+        capacidade = maxCapacidade;
+        itemArray = new Object[capacidade];
+    }
+
+    public boolean vazia(){
+        return topo == 0;
+    }
+
+    public int tamanho(){
+        return contador;
+    }
+
+    public void empilhar(Object novoItem){
+        if (topo == capacidade) {
+            System.out.println("Erro: Capacidade da Pilha foi exedida!");
+        } else {
+            itemArray[topo++] = novoItem;
+            contador++;
+        }
+    }
+
+    public Object desemplihar() {
+        Object x = null;
+        if(vazia()){
+            System.out.println("ERRO: Pilha vazia!");
+        }else{
+            x = itemArray[ --topo];
+            contador--;
+        }
+        return x;
+    }
+
+    public Object consultarTopo() {
+        Object x = null;
+        if(vazia()){
+            System.out.println("ERRO: Pilha vazia!");
+        }else{
+            x = itemArray[topo - 1];
+        }
+        return x;
+    }
+
+    public String toString() {
+        String pilhaCompleta = "";
+        if (vazia()) {
+            pilhaCompleta = null;
+        } else {
+            for (int i = topo -1; i >= 0; i--) {
+                pilhaCompleta = pilhaCompleta + "\n" + itemArray[i];
+            }
+        }
+        return pilhaCompleta;
+    }
+}
---
+++ Algoritmos2/Pilha/TestePilha.java
@@ -0,0 +1,49 @@
+public class TestePilha {
+
+    private static PilhaEstatica pilhaE; // Instancia a classe PilhaEstatica
+    private static PilhaDinamica pilhaD; // Instancia a classe PilhaDinamica
+    
+    public static void main(String[] args) {
+        
+        System.out.println("\n>>> Pilha Estatica <<<\n");
+
+        pilhaE = new PilhaEstatica(4); // Inicializa a classe Pilha Estatica
+
+        System.out.println("Topo da pilha: " + pilhaE.consultarTopo());
+        System.out.println("A pilha esta vazia? "+ pilhaE.vazia());
+
+        pilhaE.empilhar("Item 1");
+        pilhaE.empilhar(3.14);
+        pilhaE.empilhar(21);
+        System.out.println("Topo da pilha: " + pilhaE.consultarTopo());
+        pilhaE.empilhar("Item 2");
+        pilhaE.empilhar(99.78);
+        System.out.println("Conteúdo da pilha: " + pilhaE.toString());
+        System.out.println("Tamanho da pilha: "+ pilhaE.tamanho());
+        pilhaE.desemplihar();
+        System.out.println("Conteúdo da pilha: " + pilhaE.toString());
+        System.out.println("Tamanho da pilha: " + pilhaE.tamanho());
+
+        //=============================================================================
+
+        System.out.println("\n>>> Pilha Dinamica <<<\n");
+
+        pilhaD = new PilhaDinamica(); // Construtor da Pilha Dinamica
+
+
+        System.out.println("Topo da pilha: "+ pilhaD.consultarTopo());
+        System.out.println("A pilha esta vazia? "+ pilhaD.vazia());
+        pilhaD.empilhar("Item 1");
+        pilhaD.empilhar(3.14);
+        pilhaD.empilhar(21);
+        System.out.println("Topo da pilha: " + pilhaD.consultarTopo());
+        pilhaD.empilhar("Item 2");
+        pilhaD.empilhar(99.78);
+        System.out.println("Conteudo da pilha: "+ pilhaD.toString());
+        System.out.println("Tamanho da pilha: " + pilhaD.tamanho());
+        pilhaD.desempilhar();
+        System.out.println("Conteudo da pilha: " + pilhaD.toString());
+        System.out.println("Tamanho da pilha: " + pilhaD.tamanho());
+
+    }
+}
