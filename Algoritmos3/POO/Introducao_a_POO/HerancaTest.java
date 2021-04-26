b0e52004939aed1ac34f5e6d6b3e85cf99591948
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2021-04-26 09:37:21 -0300

    Movendo os arquivos para uma nova pasta
    Separando os arquivos para aprendizado dos arquivos do projeto ControleDeVendas

--- Algoritmos3/POO/Introducao_a_POO/HerancaTest.java
+++ Algoritmos3/POO/Introducao_a_POO/HerancaTest.java
@@ -1,13 +1,12 @@
-public class Main {
+public class HerancaTest {
     public static void main(String[] args) {
-        
+
         /* TESTE DA CLASSE CommissionEmployee */
 
         System.out.println("\n\tTESTE DA CLASSE CommissionEmployee\n");
 
-        // instancia o objeto CommissionEmploye 
-        CommissionEmployee employee = new CommissionEmployee(
-            "Sue", "Jones", "222-22-2222", 1000, 0.06 );
+        // instancia o objeto CommissionEmploye
+        CommissionEmployee employee = new CommissionEmployee("Sue", "Jones", "222-22-2222", 1000, 0.06);
 
         // obtém os dados de empregado comissionado
         System.out.println("Employee information obtained by get methods:");
@@ -25,19 +24,18 @@ public class Main {
 
         /* FIM TESTE DA CLASSE CommissionEmployee */
 
-
         /* TESTE DA CLASSE BasePlusCommissionEmployee */
 
         System.out.println("\n\tTESTE DA CLASSE BasePlusCommissionEmployee\n");
 
-        BasePlusCommissionEmployee baseEmployee = 
-            new BasePlusCommissionEmployee("Bob", "Lewis", "333-33-3333", 5000, .04, 300);
+        BasePlusCommissionEmployee baseEmployee = new BasePlusCommissionEmployee("Bob", "Lewis", "333-33-3333", 5000,
+                .04, 300);
 
         // Obtém os dados do empregado comissionado com salário base
         System.out.println("Employee information obtained by get methods:\n");
-        System.out.printf("%s %s%n",    "First name is", baseEmployee.getFirstName());
-        System.out.printf("%s %s%n",    "Last name is", baseEmployee.getLastName());
-        System.out.printf("%s %s%n",   "Social security number is", baseEmployee.getSocialSecurityNumber());
+        System.out.printf("%s %s%n", "First name is", baseEmployee.getFirstName());
+        System.out.printf("%s %s%n", "Last name is", baseEmployee.getLastName());
+        System.out.printf("%s %s%n", "Social security number is", baseEmployee.getSocialSecurityNumber());
         System.out.printf("%s %.2f%n", "Gross sales is", baseEmployee.getGroosSales());
         System.out.printf("%s %.2f%n", "Base salary is", baseEmployee.getBaseSalary());
 
